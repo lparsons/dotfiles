2a64c793af6e67365165358dfce21948cd7d0b0a
Lance Parsons <lparsons@princeton.edu>
2021-01-13 17:21:37 -0500

    Add UCSC Kent Tools config

--- install.sh
+++ install.sh
@@ -3,29 +3,32 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 #get_release.sh
-ln -sf ${BASEDIR}/.get_release.sh ~/.get_release.sh
+ln -sf "${BASEDIR}/.get_release.sh" "${HOME}/.get_release.sh"
 
 # vim
-ln -sf ${BASEDIR}/.vimrc ~/.vimrc
+ln -sf "${BASEDIR}/.vimrc" "${HOME}/.vimrc"
 # ln -s ${BASEDIR}/vim/ ~/.vim
 
 # nvim
-mkdir -p ${HOME}/.config/nvim
-ln -sf ${BASEDIR}/.config/nvim/init.vim ${HOME}/.config/nvim/init.vim
+mkdir -p "${HOME}/.config/nvim"
+ln -sf "${BASEDIR}/.config/nvim/init.vim" "${HOME}/.config/nvim/init.vim"
 
 # bash
-ln -sf ${BASEDIR}/.inputrc ~/.inputrc
+ln -sf "${BASEDIR}/.inputrc" "${HOME}/.inputrc"
 
 # git
-ln -sf ${BASEDIR}/.gitconfig ~/.gitconfig
+ln -sf "${BASEDIR}/.gitconfig" "${HOME}/.gitconfig"
 
 # hg
-ln -sf ${BASEDIR}/.hgrc ~/.hgrc
+ln -sf "${BASEDIR}/.hgrc" "${HOME}/.hgrc"
 
 # git
-ln -sf ${BASEDIR}/.tmux.conf ~/.tmux.conf
-ln -sf ${BASEDIR}/.tmux-osx.conf ~/.tmux-osx.conf
+ln -sf "${BASEDIR}/.tmux.conf" "${HOME}/.tmux.conf"
+ln -sf "${BASEDIR}/.tmux-osx.conf" "${HOME}/.tmux-osx.conf"
 
 # ssh
-mkdir -p ${HOME}/.ssh
-ln -sf ${BASEDIR}/.ssh/rc ${HOME}/.ssh/rc
+mkdir -p "${HOME}/.ssh"
+ln -sf "${BASEDIR}/.ssh/rc" "${HOME}/.ssh/rc"
+
+# UCSC Kent Tools
+ln -sf "${BASEDIR}/.hg.conf" "${HOME}/.hg.conf"
